3289bccf82162b86399109e3f910ed9b24e4407d
amartynov <amartynov@itcwin.com>
2010-03-02 15:35:42 +0300

    bla

--- src/org/charts3d/scatter/MyJzy3d.java
+++ src/org/charts3d/scatter/MyJzy3d.java
@@ -1,7 +1,8 @@
 package org.charts3d.scatter;
 
-import java.awt.Component;
 import java.awt.Label;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import javax.media.opengl.glu.GLU;
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -85,43 +87,49 @@ public class MyJzy3d extends JApplet {
      */
     setSize(MAINWINDOWWIDTH, MAINWINDOWHEIGHT);
     setLayout(null);
-
-   /* addNewChart("<?xml version=\"1.0\" encoding=\"utf-8\"?>"+
-
-"<plotdata xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""+
-  " xsi:noNamespaceSchemaLocation=\"coords.xsd\">"+
-
-  "<data id='1'>"+
-    "<values axis='x' name='XCOORDS' type='int'>"+
-      "<value>1</value>"+
-      "<value>4</value>"+
-      "<value>1</value>"+
-      "<value>3</value>"+
-      "<value>10</value>"+
-      "<value>5</value>"+
-    "</values>"+
-
-    "<values axis='y' name='YCOORDS' type='double'>"+
-      "<value>2.0</value>"+
-      "<value>1.0</value>"+
-      "<value>19.2</value>"+
-      "<value>45.1</value>"+
-      "<value>1.28</value>"+
-      "<value>5.55</value>"+
-    "</values>"+
-
-    "<values axis='z' name='ZCOORDS' type='double'>"+
-      "<value>5.0</value>"+
-      "<value>4.0</value>"+
-      "<value>13.2</value>"+
-      "<value>15.1</value>"+
-      "<value>1.28</value>"+
-      "<value>5.0</value>"+
-    "</values>"+
-  "</data>"+
-
-"</plotdata>");*/
     
+    JLabel greeting=new JLabel("Please, click button");
+    greeting.setBounds(100, 100, 150, 25);
+    
+    add(greeting);
+    
+    /*JButton but=new JButton("Click me");
+    but.setBounds(100, 150, 150, 25);
+    add(but);
+    but.addMouseListener(new MouseListener() {
+      
+      @Override
+      public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+      }
+      
+      @Override
+      public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+      }
+      
+      @Override
+      public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+      }
+      
+      @Override
+      public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+      }
+      
+      @Override
+      public void mouseClicked(MouseEvent e) {
+         
+        
+      }
+    });*/
+
+        
    /* ArrayList<Double>[] dCoord = coordsArray.get(0).getCoords();
     int kol = dCoord[0].size();
     coord = new Coord3d[kol];
@@ -153,6 +161,131 @@ public class MyJzy3d extends JApplet {
     JComponent component = (JComponent) chart.getCanvas();
     component.setBounds(0, 0, CHARTWIDTH, CHARTHEIGHT);
 
+    /*
+     * infoPanel settings
+     */
+   /* Label infoLabel = new Label("Points info");
+    infoLabel.setAlignment(Label.CENTER);
+    infoLabel.setBounds(0, 0, 100, 25);
+
+    infoPanel = new JPanel();
+    infoPanel.setLayout(null);
+    infoPanel.add(infoLabel);
+    infoPanel.setBounds(CHARTWIDTH + GAP, 0, INFOPANELWIDTH, INFOPANELHEIGHT);
+
+    /*
+     * pointsList settings
+     */
+   /* pointsList = new JList();
+    pointsList.addMouseListener(new PointsListMouseListener(chart, selectedPoints, pointsNumber, sc));
+
+    /*
+     * pointsScrollPane settings
+     */
+   /* JScrollPane pointsScrollPane = new JScrollPane(pointsList);
+    pointsScrollPane.setBounds(0, 28, INFOPANELWIDTH - 20, 250);
+
+    /*
+     * slider settings
+     */
+   /* JSlider slider = new JSlider(0, 10);
+    slider.setValue(POINTSIZE);
+    slider.setBounds(CHARTWIDTH + GAP, INFOPANELHEIGHT + GAP, SLIDERWIDTH, SLIDERHEIGHT);
+    slider.setMajorTickSpacing(5);
+    slider.setMinorTickSpacing(1);
+    slider.setPaintLabels(true);
+    slider.setPaintTicks(true);
+    slider.setFocusable(false);
+    slider.addChangeListener(new SplitChangeListener(chart, sc));
+
+    /*
+     * ShowHideButton settings
+     */
+    /*JButton button = new JButton("Hide");
+    button.setBounds((CHARTWIDTH - BUTTONWIDTH) / 2, CHARTHEIGHT + GAP, BUTTONWIDTH, BUTTONHEIGHT);
+    button.setFocusable(false);
+    button.addMouseListener(new ShowHideButtonListener(chart, pointsList, pointsNumber, sc));
+
+    infoPanel.add(pointsScrollPane);
+   // JComponent comp=new JComponent() {
+    //};
+    add(comp, 0);
+    add(button, 1);
+    add(infoPanel, 2);
+    add(slider, 3);*/
+    
+    //add(new JButton("Hello"));
+    
+    addNewChart("<?xml version=\"1.0\" encoding=\"utf-8\"?>"+
+
+        "<plotdata xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""+
+          " xsi:noNamespaceSchemaLocation=\"coords.xsd\">"+
+
+          "<data id='1'>"+
+            "<values axis='x' name='XCOORDS' type='int'>"+
+              "<value>1</value>"+
+              "<value>4</value>"+
+              "<value>1</value>"+
+              "<value>3</value>"+
+              "<value>10</value>"+
+              "<value>5</value>"+
+            "</values>"+
+
+            "<values axis='y' name='YCOORDS' type='double'>"+
+              "<value>2.0</value>"+
+              "<value>1.0</value>"+
+              "<value>19.2</value>"+
+              "<value>45.1</value>"+
+              "<value>1.28</value>"+
+              "<value>5.55</value>"+
+            "</values>"+
+
+            "<values axis='z' name='ZCOORDS' type='double'>"+
+              "<value>5.0</value>"+
+              "<value>4.0</value>"+
+              "<value>13.2</value>"+
+              "<value>15.1</value>"+
+              "<value>1.28</value>"+
+              "<value>5.0</value>"+
+            "</values>"+
+          "</data>"+
+
+        "</plotdata>");//*/
+
+
+  }
+  public void addNewChart(String xmlString){
+    
+    //removeAll();
+    
+    ByteArrayInputStream bais = new ByteArrayInputStream(xmlString.getBytes());
+    try {
+      coordsArray = (ArrayList<PlotStorage>) XMLparser.parse(bais);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    ArrayList<Double>[] dCoord = coordsArray.get(0).getCoords();
+    int kol = dCoord[0].size();
+    coord = new Coord3d[kol];
+    color = new Color[coord.length];
+    for (int i = 0; i < kol; i++){
+      coord[i] = new Coord3d(dCoord[0].get(i), dCoord[1].get(i), dCoord[2].get(i));
+      color[i] = new Color(0,0 , (float)1);
+    }
+    
+    sc = new MySelectableScatter(coord, color) {
+      public void draw(GL gl, GLU glu, Camera cam) {
+        gl.glEnable(GL.GL_POINT_SMOOTH); // should be in init
+        super.draw(gl, glu, cam);
+      }
+    };
+    MyChart m = new MyChart(sc);
+    chart = m.getChart();
+
+    JComponent component = (JComponent) chart.getCanvas();
+    component.setBounds(0, 0, CHARTWIDTH, CHARTHEIGHT);
+    
     /*
      * infoPanel settings
      */
@@ -199,48 +332,15 @@ public class MyJzy3d extends JApplet {
     button.addMouseListener(new ShowHideButtonListener(chart, pointsList, pointsNumber, sc));
 
     infoPanel.add(pointsScrollPane);
-    JComponent comp=new JComponent() {
-    };
-    add(comp, 0);
+    //JComponent comp=new JComponent() {
+    //};
+    //add(comp, 0);
+    add(component, 0);
     add(button, 1);
     add(infoPanel, 2);
     add(slider, 3);
     
-   // add(new JButton("Hello"));
-
-  }
-  public void addNewChart(String xmlString){
-    ByteArrayInputStream bais = new ByteArrayInputStream(xmlString.getBytes());
-    try {
-      coordsArray = (ArrayList<PlotStorage>) XMLparser.parse(bais);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    chart.getCanvas().forceRepaint();
     
-    ArrayList<Double>[] dCoord = coordsArray.get(0).getCoords();
-    int kol = dCoord[0].size();
-    coord = new Coord3d[kol];
-    color = new Color[coord.length];
-    for (int i = 0; i < kol; i++){
-      coord[i] = new Coord3d(dCoord[0].get(i), dCoord[1].get(i), dCoord[2].get(i));
-      color[i] = new Color(0, (float) 1, 0);
-    }
-    
-    sc = new MySelectableScatter(coord, color) {
-      public void draw(GL gl, GLU glu, Camera cam) {
-        gl.glEnable(GL.GL_POINT_SMOOTH); // should be in init
-        super.draw(gl, glu, cam);
-      }
-    };
-    MyChart m = new MyChart(sc);
-    chart = m.getChart();
-    
-
-    
-
-    JComponent component = (JComponent) chart.getCanvas();
-    component.setBounds(0, 0, CHARTWIDTH, CHARTHEIGHT);
-    
-    add(component, 0);
   }
 }
