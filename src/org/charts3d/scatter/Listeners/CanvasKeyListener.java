c3917ad57abbe77881304f03636c4b03effb50e3
amartynov <amartynov@itcwin.com>
2010-03-02 12:33:34 +0300

    add key listener

---
+++ src/org/charts3d/scatter/Listeners/CanvasKeyListener.java
@@ -0,0 +1,47 @@
+package org.charts3d.scatter.Listeners;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import org.charts3d.scatter.MyScatterSelector;
+
+import net.masagroup.jzy3d.chart.Chart;
+import net.masagroup.jzy3d.chart.controllers.mouse.ChartMouseController;
+import net.masagroup.jzy3d.chart.controllers.mouse.ScatterMouseSelector;
+
+public class CanvasKeyListener implements KeyListener{
+  private boolean isShiftPressed=false;
+  private Chart chart=null;
+  private MyScatterSelector mouseSelection=null;
+  private ChartMouseController mouseCamera=null;
+  
+  public CanvasKeyListener(Chart chart,ScatterMouseSelector mouseSelection,ChartMouseController mouseCamera){
+    this.chart=chart;
+    this.mouseSelection=(MyScatterSelector)mouseSelection;
+    this.mouseCamera=mouseCamera;
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+      isShiftPressed=false;
+      mouseSelection.releaseChart();
+      chart.addController(mouseCamera);
+    }
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    if (!isShiftPressed && e.isShiftDown()) {
+      isShiftPressed=true;
+      chart.removeController(mouseCamera);
+      mouseSelection.attachChart(chart);
+
+    } 
+  }
+}
